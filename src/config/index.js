ca3545aa5d0eead789b30e5c28424ec579f2a628
lukas1013 <leonardolukas10@gmail.com>
2021-06-12 10:59:53 -0300

    fixes reverse geocode API
    add API with ssl to solve security issue

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,12 @@
 const config = {
     geocoding: {
         geocode: {
-            api: 'https://geocode.search.hereapi.com/v1/geocode?q=',
-            api_key: process.env.REACT_APP_GEOCODE_API_KEY
+            api: 'https://geocode.search.hereapi.com/v1/geocode?q='
         },
         reverse: {
-            api: 'http://api.positionstack.com/v1/reverse',
-            access_key: process.env.REACT_APP_REVERSE_API_KEY
-        }
+            api: 'https://revgeocode.search.hereapi.com/v1/revgeocode'
+        },
+        api_key: process.env.REACT_APP_GEOCODING_API_KEY
     },
     weather: {
         api: 'https://api.openweathermap.org/data/2.5/onecall',
